56b46c85993ce7c95df79040190e73fd7d2d3a0b
weswinggg <weslee81412@gmail.com>
2021-07-10 15:03:33 +0800

    add dpet price

--- commands/prices.js
+++ commands/prices.js
@@ -6,47 +6,51 @@ module.exports = {
     const slp = "smooth-love-potion";
     const eth = "ethereum";
     const axs = "axie-infinity";
+    const dpet = "my-defi-pet";
     const currency = "php";
     const fetch = require('node-fetch');
 
+    let price;
     // return current prices for ETH SLP AXS
     if(!args[0]) {
-      let prices = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${eth},${slp},${axs},$&vs_currencies=${currency}`).then(response => response.json());
+      let prices = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${eth},${slp},${axs},${dpet}$&vs_currencies=${currency}`).then(response => response.json());
 
       let msg = `1 ETH= ${prices[eth][currency]} ${currency.toUpperCase()}\n`;
       msg += `1 AXS = ${prices[axs][currency]} ${currency.toUpperCase()}\n`;
       msg += `1 SLP = ${prices[slp][currency]} ${currency.toUpperCase()}`;
-      message.channel.send(msg);
+      msg += `1 DPET = ${prices[dpet][currency]} ${currency.toUpperCase()}`;
+      return message.channel.send(msg);
     }
 
     // return SLP price
     else if(args[0] === "slp") {
-      let price;
       await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${slp}&vs_currencies=${currency}`).then(response => response.json()).then(data => {
         price = data[slp][currency];
       });
-
-      message.channel.send(`1 ${args[0].toUpperCase()} = ${price} ${currency.toUpperCase()}`);
     }
 
     // return ETH price
     else if(args[0] === "eth") {
-      let price;
       await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${eth}&vs_currencies=${currency}`).then(response => response.json()).then(data => {
         price = data[eth][currency];
       });
-
-      message.channel.send(`1 ${args[0].toUpperCase()} = ${price} ${currency.toUpperCase()}`);
     }
 
     // return AXS price
     else if(args[0] === "axs") {
-      let price;
       await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${axs}&vs_currencies=${currency}`).then(response => response.json()).then(data => {
         price = data[axs][currency];
       });
-
-      message.channel.send(`1 ${args[0].toUpperCase()} = ${price} ${currency.toUpperCase()}`);
     }
+
+    else if(args[0] === "dpet") {
+      await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${dpet}&vs_currencies=${currency}`).then(response => response.json()).then(data => {
+        price = data[axs][currency];
+      });
+    }
+
+    else return;
+
+    return message.channel.send(`1 ${args[0].toUpperCase()} = ${price} ${currency.toUpperCase()}`);;
   }
 }
